f213b2dd1301f192f2227713cd75d2a6baee0fa7
Bertrik Sikken <bertrik@sikken.nl>
2026-02-28 12:54:20 +0100

    Add hash calculation code to stm32wle5.

--- sensor/stm32wle5/stm32wle5.ino
+++ sensor/stm32wle5/stm32wle5.ino
@@ -4,6 +4,8 @@
 #include <Arduino.h>
 #include <RadioLib.h>
 #include <MiniShell.h>
+#include <Crypto.h>
+#include <BLAKE2s.h>
 
 #define printf Serial.printf
 
@@ -61,9 +63,57 @@ static int do_receive(int argc, char *argv[])
     return result;
 }
 
+static void printhex(const char *title, const uint8_t *buf, size_t len)
+{
+    printf("%s", title);
+    for (size_t i = 0; i < len; i++) {
+        if ((i % 16) == 0) {
+            printf("\n%04X:", i);
+        }
+        printf(" %02X", buf[i]);
+    }
+    printf("\n");
+}
+
+static int do_key(int argc, char *argv[])
+{
+    uint32_t deviceid = HAL_GetUIDw0() ^ HAL_GetUIDw1() ^ HAL_GetUIDw2();
+    uint8_t deviceid_buf[4];
+    deviceid_buf[0] = deviceid >> 24;
+    deviceid_buf[1] = deviceid >> 16;
+    deviceid_buf[2] = deviceid >> 8;
+    deviceid_buf[3] = deviceid >> 0;
+    if (argc < 3) {
+        printf("Device id = 0x%08X\n", deviceid);
+        printhex("Device id", deviceid_buf, 4);
+        return 0;
+    }
+
+    char *name = argv[1];
+    char *secret = argv[2];
+    printf("name=%s, secret=%s\n", name, secret);
+
+    BLAKE2s blake;
+    uint8_t device_key[32];
+    blake.reset(secret, strlen(secret));
+    blake.update(name, strlen(name));
+    blake.update(deviceid_buf, sizeof(deviceid_buf));
+    blake.finalize(device_key, 32);
+    printhex("Key", device_key, 32);
+    return 0;
+}
+
+static int do_reboot(int argc, char *argv[])
+{
+    NVIC_SystemReset();
+    return 0;
+}
+
 const cmd_t commands[] = {
     { "init", do_init, "Initialise the radio" },
     { "receive", do_receive, "Start receiving" },
+    { "key", do_key, "<name> <secret> Create key" },
+    { "reboot", do_reboot, "Reboot" },
     { NULL, NULL, NULL }
 };
 
@@ -85,7 +135,8 @@ void loop(void)
     if (rf_received) {
         int num_bytes = radio.getPacketLength();
         int state = radio.readData(rf_buffer, num_bytes);
-        printf("Got %d bytes\n", num_bytes);
+        printf("Got %3d bytes, ", num_bytes);
+        printhex("data:", rf_buffer, num_bytes);
         rf_received = false;
     }
 }
